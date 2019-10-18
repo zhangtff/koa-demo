707d399a9e4911a6e14fab5562653b8ba7d26e16
张亮(liang.zhang) <liang.zhang@inspur.com>
2019-10-18 20:13:45 +0800

    koa 动态路由和中间件学习

---
+++ ejs-index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa')
+const router = require('koa-router')()
+const views = require('koa-views')
+
+const app = new Koa()
+
+
+
+
+app.listen(3000, () => {
+    Console.log()
+})
--- index.js
+++ index.js
@@ -7,16 +7,32 @@ const Router = require('koa-router')
 const app = new Koa()
 const router = new Router()
 
+// 应用级中间件,每个访问都输出当前时间，app.use无论代码在哪，都在路由匹配前执行
+app.use(async (ctx, next) => {
+    console.log(new Date())
+    // ctx.body = '这是一个中间件'
+    await next() // 继续向下运行
+    if (ctx.status === 404) {
+        ctx.body = '这是个404页面'
+    }
+})
+
 // 配置路由
 router.get('/', async (ctx) => {
     ctx.body = '这是首页'
 })
 router.get('/news', async (ctx) => {
+    console.log('这是一个新闻')
     // 获取get传值
     console.log(ctx.query)
     console.log(ctx.querystring)
     console.log(ctx.request.url)
     ctx.body = '这是新闻页'
+})
+// 动态路由,可以多层嵌套
+router.get('/newscontent/:id/:cid', async (ctx) => {
+    console.log(ctx.params)
+    ctx.body = `这是新闻${ctx.params.id}-${ctx.params.cid}内容页`
 })
 
 // 启动路由
